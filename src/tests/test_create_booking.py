b80a8c0781b2873436eb489e62be00750140fc35
Egor Zenzin <zenzin48@mail.ru>
2024-11-25 10:49:16 +0600

    Исправление теста

--- src/tests/test_create_booking.py
+++ src/tests/test_create_booking.py
@@ -26,12 +26,24 @@ class TestCreateBooking:
         }
 
         response = BookingAPI.create_booking(data)
-
         assert response.status_code == 200
+
         response_data = response.json()
         assert response_data['bookingid'] is not None
         assert response_data['booking']['firstname'] == "Egor"
 
         booking_id = response_data['bookingid']
+
+        get_response = BookingAPI.get_booking(booking_id)
+        assert get_response.status_code == 200
+        get_response_data = get_response.json()
+        assert get_response_data['firstname'] == "Egor"
+        assert get_response_data['lastname'] == "Zenzin"
+        assert get_response_data['totalprice'] == 150
+        assert get_response_data['depositpaid'] is True
+        assert get_response_data['bookingdates']['checkin'] == "2024-01-01"
+        assert get_response_data['bookingdates']['checkout'] == "2024-01-15"
+        assert get_response_data['additionalneeds'] == "Breakfast"
+
         delete_response = BookingAPI.delete_booking(booking_id, token)  
         assert delete_response.status_code == 403  
